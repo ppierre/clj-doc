20005ca48119634cfe089e164d0594120e83d2c8
Pierre Pracht <pierre.pracht@gmail.com>
2008-12-12 00:33:21 +0100

    Remove clojure.contrib.pred

--- bin/gen.clj
+++ bin/gen.clj
@@ -5,8 +5,7 @@
   (clj-doc.core/generate-and-write
     `(clojure.core clojure.set clojure.xml
       clojure.contrib.cond  clojure.contrib.def  clojure.contrib.duck-streams
-      clojure.contrib.fcase clojure.contrib.pred clojure.contrib.seq-utils
-      clojure.contrib.str-utils)
+      clojure.contrib.fcase clojure.contrib.seq-utils clojure.contrib.str-utils)
     `(~clojure-src,
       ~clojure-contrib-src)
     #(clj-doc.generator.default/generate  %1 %2 static-contents-dir
